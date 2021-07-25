f87ba4eac1821c8470db7cfb037e0cc48992deae
Muhammad Danish <79780670+Muhammad-Danish-31@users.noreply.github.com>
2021-07-25 15:33:29 +0500

    ASSIGNMENT

---
+++ java.js
@@ -0,0 +1,96 @@
+//  var itemsArray =[
+//     {name:"juice",price:50, quantity:3},
+//     {name:"cookie",price:30, quantity:9},
+//     {name:"shirt",price:880, quantity:1},
+//     {name:"pen",price:100, quantity:2}
+// ];
+// console.log(itemsArray);
+
+// var array1=(itemsArray[0].price+itemsArray[0].quantity);
+// var array2=(itemsArray[1].price+itemsArray[1].quantity);
+// var array3=(itemsArray[2].price+itemsArray[2].quantity);
+// var array4=(itemsArray[3].price+itemsArray[3].quantity);
+
+//  console.log(array1);
+//  console.log(array2);
+//  console.log(array3);
+//  console.log(array4);
+
+
+//__________________________________________________________________
+
+/*var obj={
+ Firstname: "Muhammad",
+ Lastname: "Danish",
+ Email:"b18158031.muhammaddanish@gmail.com",
+ Password:1234,
+ Gender: "male",
+ City:"karachi",
+ country :"pakistan" ,
+ age:19
+}
+console.log(obj);
+
+var obj= prompt ("asked")
+
+if (obj==age){
+    alert(valid);
+
+}
+else if (obj==country)
+{
+ alert(valid);
+}
+else
+{
+    alert(invlaid);
+}*/
+
+//___________________________________________
+
+
+// function Plan(age ,name , fathername)
+// {
+//     this.age = age ;
+//     this.name = name;
+//     this.fathername = fathername;
+// }
+
+// var obj1= new Plan(21,"Danish","Abdul Rauf");
+// var obj2= new Plan(19,"Ähmer","Abdul Rauf");
+
+// console.log(obj1,obj2);
+
+//_________________________________
+
+// function Myfun(name , gender ,education, address , profession)
+// {
+//     this.name =address;
+//     this.gender=address;
+//     this.eucation=address;
+//     this. address=address;
+//     this.profession=address;
+// }
+
+// function fun()
+// {
+//     var name = document.getElementById("name").value;
+//     var address = document.getElementById("address").value;
+//     var gender = document.getElementById("gender").value;
+//     var education= document.getElementById("education").value;
+//     var profession = document.getElementById("profession").value;
+
+// var fun1 = new Myfun("danish","male","graduation","xyz","developer"); 
+
+// console.log(fun1);
+
+// }
+
+
+
+
+
+
+
+
+
