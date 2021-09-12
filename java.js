a15d165fa54a87287af396682f75d134419b434f
Muhammad Danish <79780670+Muhammad-Danish-31@users.noreply.github.com>
2021-09-12 02:50:03 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,25 +1,25 @@
-//  var itemsArray =[
-//     {name:"juice",price:50, quantity:3},
-//     {name:"cookie",price:30, quantity:9},
-//     {name:"shirt",price:880, quantity:1},
-//     {name:"pen",price:100, quantity:2}
-// ];
-// console.log(itemsArray);
+ var itemsArray =[
+     {name:"juice",price:50, quantity:3},
+    {name:"cookie",price:30, quantity:9},
+     {name:"shirt",price:880, quantity:1},
+   {name:"pen",price:100, quantity:2}
+];
+console.log(itemsArray);
 
-// var array1=(itemsArray[0].price+itemsArray[0].quantity);
-// var array2=(itemsArray[1].price+itemsArray[1].quantity);
-// var array3=(itemsArray[2].price+itemsArray[2].quantity);
-// var array4=(itemsArray[3].price+itemsArray[3].quantity);
+ var array1=(itemsArray[0].price+itemsArray[0].quantity);
+ var array2=(itemsArray[1].price+itemsArray[1].quantity);
+ var array3=(itemsArray[2].price+itemsArray[2].quantity);
+ var array4=(itemsArray[3].price+itemsArray[3].quantity);
 
-//  console.log(array1);
-//  console.log(array2);
-//  console.log(array3);
-//  console.log(array4);
+ console.log(array1);
+  console.log(array2);
+  console.log(array3);
+  console.log(array4);
 
 
-//__________________________________________________________________
+__________________________________________________________________
 
-/*var obj={
+var obj={
  Firstname: "Muhammad",
  Lastname: "Danish",
  Email:"b18158031.muhammaddanish@gmail.com",
@@ -44,47 +44,45 @@ else if (obj==country)
 else
 {
     alert(invlaid);
-}*/
+}
 
-//___________________________________________
+___________________________________________
 
 
-// function Plan(age ,name , fathername)
-// {
-//     this.age = age ;
-//     this.name = name;
-//     this.fathername = fathername;
-// }
+ function Plan(age ,name , fathername)
+ {
+     this.age = age ;
+     this.name = name;
+     this.fathername = fathername;
+ }
+var obj1= new Plan(21,"Danish","Abdul Rauf");
+ var obj2= new Plan(19,"Ähmer","Abdul Rauf");
 
-// var obj1= new Plan(21,"Danish","Abdul Rauf");
-// var obj2= new Plan(19,"Ähmer","Abdul Rauf");
+ console.log(obj1,obj2);
 
-// console.log(obj1,obj2);
+_________________________________
 
-//_________________________________
+ function Myfun(name , gender ,education, address , profession)
+{
+    this.name =address;
+    this.gender=address;
+    this.eucation=address;
+    this. address=address;
+     this.profession=address;
+ }
 
-// function Myfun(name , gender ,education, address , profession)
-// {
-//     this.name =address;
-//     this.gender=address;
-//     this.eucation=address;
-//     this. address=address;
-//     this.profession=address;
-// }
+ function fun()
+{
+     var name = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+     var gender = document.getElementById("gender").value;
+     var education= document.getElementById("education").value;
+    var profession = document.getElementById("profession").value;
 
-// function fun()
-// {
-//     var name = document.getElementById("name").value;
-//     var address = document.getElementById("address").value;
-//     var gender = document.getElementById("gender").value;
-//     var education= document.getElementById("education").value;
-//     var profession = document.getElementById("profession").value;
+ var fun1 = new Myfun("danish","male","graduation","xyz","developer"); 
+ console.log(fun1);
 
-// var fun1 = new Myfun("danish","male","graduation","xyz","developer"); 
-
-// console.log(fun1);
-
-// }
+ }
 
 
 
